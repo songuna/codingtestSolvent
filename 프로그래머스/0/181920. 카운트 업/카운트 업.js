c7925eb6f9371d6b46c5b8cd272b8709a44e9411
SONG _YOU_NA <138658065+songuna@users.noreply.github.com>
2024-02-13 17:27:08 +0900

    [level 0] Title: 카운트 업, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.js
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.js
@@ -1,8 +1,4 @@
 function solution(start_num, end_num) {
-    let arr = [];
-    let sum;
-    for (sum=start_num; sum<=end_num; sum++){
-        arr.push(sum)
-    }
-    return arr;
+    var answer = [];
+    return answer;
 }
